041652c1d93baa7ce210945cffd1a76770eb5d20
jjzborowski <j.j.zborowski@gmail.com>
2017-04-03 13:53:55 +0200

    UPDATE #6

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,33 +5,35 @@ const gulp = require('gulp'),
       rename = require('gulp-rename'),
       sass = require('gulp-sass'),
       uglify = require('gulp-uglify'),
-      src = {
-          sass: 'source/sass/*.scss',
-          js: 'source/js/*.js'    
-      },
-      dest = {
-          css: 'app/css/',
-          js: 'app/js/'
+      config = {
+          src: {
+              sass: 'source/sass/main.scss',
+              js: 'source/js/*.js'    
+          },
+          dest: {
+              css: 'app/css/',
+              js: 'app/js/'
+          }
       };
 
-gulp.task('convertion-js', () => {
-    return gulp.src(src.js)
+gulp.task('convertion-js', ()=>{
+    return gulp.src(config.src.js)
         .pipe(babel({presets: ['es2015']}))
-//        .pipe(uglify())
+    //        .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(dest.js));
+        .pipe(gulp.dest(config.dest.js));
 });
 
-gulp.task('convertion-sass', () => {
-    return gulp.src(src.sass)
+gulp.task('convertion-sass', ()=>{
+    return gulp.src(config.src.sass)
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(css({compatibility: '*'}))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(dest.css));
+        .pipe(gulp.dest(config.dest.css));
 });
 
-gulp.task('default', () => {
-    gulp.watch(src.sass, ['convertion-sass']);
-    gulp.watch(src.js, ['convertion-js']);
+gulp.task('default', ()=>{
+    gulp.watch(config.src.sass, ['convertion-sass']);
+    gulp.watch(config.src.js, ['convertion-js']);
 });
