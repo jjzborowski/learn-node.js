2aa27c36e457ded0278e26eefb2c40cf789afc18
jjzborowski <j.j.zborowski@gmail.com>
2017-05-23 10:28:09 +0200

    UPDATE #8

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,7 @@ const gulp = require('gulp'),
       rename = require('gulp-rename'),
       sass = require('gulp-sass'),
       uglify = require('gulp-uglify'),
+      sourcemaps = require('gulp-sourcemaps'),
       config = {
           src: {
               sass: 'source/sass/main.scss',
@@ -18,18 +19,22 @@ const gulp = require('gulp'),
 
 gulp.task('convertion-js', ()=>{
     return gulp.src(config.src.js)
+        .pipe(sourcemaps.init())
         .pipe(babel({presets: ['es2015']}))
     //        .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.dest.js));
 });
 
 gulp.task('convertion-sass', ()=>{
     return gulp.src(config.src.sass)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(css({compatibility: '*'}))
         .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.dest.css));
 });
 
